787df8a7d8b1803c9e06f13625a8ee693366fdf8
anand9023-a <ap9023633488@gmail.com>
2025-08-05 09:40:38 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log("Main JS loaded.");
+
+// Example: show welcome alert on homepage
+if (window.location.pathname.includes("index.html")) {
+  console.log("Welcome to YoungStyle Store!");
+}
